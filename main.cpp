db34391e01026d2ac1b7420b47a0f247a8e4b6f7
jakub <sjakub@outlook.com>
2018-03-22 16:04:15 +0100

    wersja ostateczna

--- main.cpp
+++ main.cpp
@@ -3,16 +3,47 @@
 #include "other.h"
 using namespace std;
 
+#include <ctime>
+
+int compare();
+
 int main()
 {
-    int A[20];
-    aRand(A, 20, 10000);
-    for(auto e:A)
-        cout<<e<<" ";
-    cout<<endl;
-    radixSort(A, 20, 10000);
- //   decimalRadixSort(A, 20, 1000);
-    for(auto e:A)
-        cout<<e<<" ";
-  // delete[] A;
+    cout<<compare();
+}
+
+int compare(){
+    int init = 62500000;
+
+    double timeQ=1;
+    double timeR=0;
+
+    while(timeQ > timeR){
+        cout<<"init: "<<init<<endl;
+//        make array
+        auto *A = new int[init];
+        rangeArray(A, init, 10000);
+
+//        Quick Sort test
+        cout<<"quick ";
+        auto start = clock();
+        qSort(A, init);
+        auto end = clock();
+        timeQ = (double) (end - start) / CLOCKS_PER_SEC;
+
+//        Counting sort test
+        cout<<"counting";
+        start = clock();
+        radixSort(A, init, 10000);
+        end = clock();
+        timeR = (double) (end - start) / CLOCKS_PER_SEC;
+
+        init *= 5;
+
+        delete[] A;
+    }
+
+    init /= 10;
+
+    return init;
 }
--- qsort.cpp
+++ qsort.cpp
@@ -1,49 +1,42 @@
 #include "sort.h"
 #include <algorithm>
 
-int parti(int A[], int n)
-{
-    if(n>=3)
-    {
-        int a=A[0];
-        int b=A[n/2];
-        int c=A[n-1];
+int parti(int A[], int n) {
+    if (n >= 3) {
+        int a = A[0];
+        int b = A[n / 2];
+        int c = A[n - 1];
 
-        if(a<c)
-        {
-            if(a<b)
-                std::swap(A[n-1], A[0]);
-            else
-                if(b<c)
-                std::swap(A[n/2], A[0]);
+        if (a < c) {
+            if (a < b)
+                std::swap(A[n - 1], A[0]);
+            else if (b < c)
+                std::swap(A[n / 2], A[0]);
         }
     }
 
-    int pivot=A[n-1];
-    int border=-1;
+    int pivot = A[n - 1];
+    int border = -1;
 
-    int i=0;
-    while(i<n-1)
-    {
-        if(A[i] <= pivot)
-        {
+    int i = 0;
+    while (i < n - 1) {
+        if (A[i] <= pivot) {
             border++;
             std::swap(A[i], A[border]);
         }
         i++;
     }
 
-    std::swap(A[i], A[border+1]);
+    std::swap(A[i], A[border + 1]);
 
-    return border+1;
+    return border + 1;
 }
 
-void qSort(int A[], int n)
-{
-    if(n<=1)
+void qSort(int A[], int n) {
+    if (n <= 1)
         return;
 
-    int k=parti(A, n);
+    int k = parti(A, n);
     qSort(A, k);
-    qSort(A+k+1, n-(k+1));
+    qSort(A + k + 1, n - (k + 1));
 }
